04ba1e1b676893d1c4bf88be5f3bc648573b93fb
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-04-14 23:53:50 -0500

    Moved store to index.js - added Provider

--- src/App.js
+++ src/App.js
@@ -8,10 +8,6 @@ import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 import { carReducer } from './reducers/carReducer';
 
-//Step 1 - Create store and connect to the react app
-const store = createStore(carReducer);
-console.log(store);
-
 const App = () => {
 	const state = {
 		additionalPrice: 0,

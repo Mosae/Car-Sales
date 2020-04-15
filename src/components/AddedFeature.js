9732a8fecd0c5dfc5feaed4fb384d0064571bbc4
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-04-15 14:22:23 -0500

    added onclick func to added feature btn

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -8,7 +8,11 @@ const AddedFeature = (props) => {
 	return (
 		<li>
 			{/* Add an onClick to run a function to remove a feature */}
-			<button className="button" onClick={removeFeature}>
+			<button
+				className="button"
+				onClick={() => {
+					props.removeFeature(props.feature);
+				}}>
 				X
 			</button>
 			{props.feature.name}
